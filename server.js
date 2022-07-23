77799e2d1de5d0efbb90a2859731fc504549b452
Matt <mwhlavaty@gmail.com>
2022-07-23 12:49:25 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const db = require('./db/connection');
+const apiRoutes = require('./routes/apiRoutes');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//for apiRoutes
+// app.use('/api', apiRoutes);
+
+// default response for any other request (not found)
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+// start server after db connection
+db.connect(err => {
+    if (err) throw err;
+    console.log('Database connected.');
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+});
